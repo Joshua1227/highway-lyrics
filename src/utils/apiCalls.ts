b2b63edaffd17b1b972832a65c6523f8e3a2bb7b
Sandeep Joshua Daniel <sandeepjoshua.daniel@q2.com>
2025-02-08 15:42:42 +0530

    fixes to call api

--- src/utils/apiCalls.ts
+++ src/utils/apiCalls.ts
@@ -1,7 +1,4 @@
-interface TitleItem {
-    id: string
-    title: string
-}
+import { TitleItem } from "./models"
 
 // interface Titles extends Array<TitleItem> { }
 
@@ -12,14 +9,15 @@ async function GetAllTitles(): Promise<TitleItem[]> {
     const headers: Headers = new Headers()
     // Add a few headers
     headers.set('Content-Type', 'application/json')
-    headers.set('Accept', 'application/json')
+    // headers.set('Accept', 'application/json')
 
     // Create the request object, which will be a RequestInfo type. 
     // Here, we will pass in the URL as well as the options object as parameters.
-    const request: RequestInfo = new Request('http://<IPAddress>/getAllTitles', {
-        method: 'GET',
-        headers: headers
+    const request: RequestInfo = new Request('http://106.51.61.53:8080/getAllTitles', {
+        method: 'GET'
     })
+
+    console.log("DATA:",fetch)
 
     // For our example, the data is stored on a static `users.json` file
     const res = await fetch(request)
---
+++ src/utils/models.ts
@@ -0,0 +1,10 @@
+export interface TitleItem {
+    id: string
+    title: string
+}
+
+export interface Song {
+    id: string
+    title: string
+    lyrics: string
+}
